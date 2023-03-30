cb854f1c7665b313c637820ccd7573187866ee77
shenghuaxiong <67672773+shenghuaxiong@users.noreply.github.com>
2023-03-30 22:12:18 +0800

    Update stream.ts

--- src/pages/api/stream.ts
+++ src/pages/api/stream.ts
@@ -34,7 +34,7 @@ export const post: APIRoute = async context => {
     },
     method: "POST",
     body: JSON.stringify({
-      model: "gpt-4",
+      model: "gpt-3.5-turbo",
       messages,
       temperature,
       stream: true
